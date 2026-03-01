2b366fc01f1e9f00706a4a48f04bc8725dd29893
temidayo112 <teedeedesigns@yahoo.com>
2026-03-01 23:55:43 +0100

    solved

--- server.js
+++ server.js
@@ -10,17 +10,48 @@ const socket = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 
-app.use(cors({
-    origin : process.env.mode === 'pro' ? [process.env.client_customer_production_url,process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'],
-    credentials : true
-}))
+const allowedOrigins = process.env.mode === 'pro'
+ ? [process.env.client_customer_production_url, process.env.
+    client_admin_production_url]
+     : ['http://localhost:3000', 'http://localhost:3001'];
+
+    app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+}, 
+credentials: true
+}));
 
 const io = socket(server, {
     cors: {
-        origin: process.env.mode === 'pro' ? [process.env.client_customer_production_url,process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'],
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
         credentials: true
     }
-})
+});
+
+// app.use(cors({
+//     origin : process.env.mode === 'pro' ? [process.env.
+//         client_customer_production_url,process.env.
+//         client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'],
+//         credentials : true
+// }))
+
+// const io = socket(server, {
+//     cors: {
+//         origin: process.env.mode === 'pro' ? [process.env.client_customer_production_url,process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001'],
+//         credentials: true
+//     }
+// })
 
 var allCustomer = []
 var allSeller = []
